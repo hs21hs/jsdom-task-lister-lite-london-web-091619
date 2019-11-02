19a48239cb155b72f506525bb5a9a3c7395c6a96
github username <github email address>
2019-11-02 13:13:45 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
   // your code here
+one()
+
 });
+
+
+function one(){
+  //grab the form
+  const form = document.getElementById("create-task-form")
+  const value = document.getElementById("new-task-description")
+  const submit = document.getElementById("button")
+
+
+  
+  //on the click of submit for we want an event listener that>>
+    //prevents default
+    submit.addEventListener("click",function(event){
+      console.log(value.value)
+      let l = document.createElement('h1')
+      l.innerHTML= value.value
+      document.body.appendChild(l)
+
+      event.preventDefault();
+      
+      
+    })
+    //appends the input to html
+
+}
